0540fde3dd2ba1c95a893b16e908c11f847c5c2c
Nurelifkfl <nurelifkfl@gmail.com>
2021-08-14 23:52:20 +0100

    updated

---
+++ src/test/java/com/demoqa/stepDefinitions/acceptAlertStepDef.java
@@ -0,0 +1,36 @@
+package com.demoqa.stepDefinitions;
+
+import com.demoqa.utilities.ConfigurationReader;
+import com.demoqa.utilities.Driver;
+import com.demoqa.utilities.browserUtils;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class acceptAlertStepDef {
+    @Given("user is on the alerts page")
+    public void user_is_on_the_alerts_page() {
+        Driver.get().get(ConfigurationReader.get("url")+"/alerts");
+
+    }
+
+    @When("user click the second button")
+    public void user_click_the_second_button() {
+        WebElement alertButton = Driver.get().findElement(By.xpath("//*[@id='timerAlertButton']"));
+       alertButton.click();
+        browserUtils.waitFor(5);
+
+    }
+
+    @Then("user should be able to accept alert")
+    public void user_should_be_able_to_accept_alert() {
+
+        Alert alert=Driver.get().switchTo().alert();
+        alert.accept();
+
+    }
+
+}
